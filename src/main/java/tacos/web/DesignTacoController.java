86110ccb19b422dd181e0895efe00c0515557423
Phuong Nguyen <phuongsover2@gmail.com>
2023-07-23 23:43:45 +0700

    create thymeleaf template to create design for taco object

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.domain.Ingredient;
 import tacos.domain.Ingredient.Type;
@@ -37,6 +38,14 @@ public class DesignTacoController {
     }
     model.addAttribute("design", new Taco());
     return "design";
+  }
+
+  @PostMapping
+  public String processDesign(Taco design){
+    // Save the taco design
+    // We'll do this in chapter 3
+    log.info("Processing design: " + design);
+    return "redirect:/orders/current";
   }
 
   private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
