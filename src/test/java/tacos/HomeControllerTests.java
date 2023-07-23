d0b598141010ab7641c5edc482d103351d709ecc
Phuong Nguyen <phuongsover2@gmail.com>
2023-07-23 23:33:09 +0700

    initial commit

---
+++ src/test/java/tacos/HomeControllerTests.java
@@ -0,0 +1,27 @@
+package tacos;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+  @Autowired private MockMvc mockMvc;
+
+  @Test
+  void testHome() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.get("/"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.view().name("home"))
+        .andExpect(
+            MockMvcResultMatchers.content().string(Matchers.containsString(("Welcome to..."))));
+  }
+}
